7881119f52bfb241b895b70287328781abcca01a
moapochta06 <90915948+moapochta06@users.noreply.github.com>
2024-10-28 23:07:57 +0700

    join templates

--- pollster/settings.py
+++ pollster/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,8 +42,9 @@ ROOT_URLCONF = 'pollster.urls'
 
 TEMPLATES = [
     {
+        # make changes in DIRS[].
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
